532405f8f165741a06a7fcddf17b56e37a37c826
yago batista da silva <yagobatistasilva@gmail.com>
2019-08-05 23:34:06 -0300

    seja o que deus quiser acho q isso tudo ta errado

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'wolpe' );
 define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', "coffee2751*" );
+define( 'DB_PASSWORD', "" );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
