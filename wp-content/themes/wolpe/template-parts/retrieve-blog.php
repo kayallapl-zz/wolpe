a9aa9de635e9374655ce24565d9ee2eca34f8587
yago batista da silva <yagobatistasilva@gmail.com>
2019-08-15 21:57:55 -0300

    paginas de single

---
+++ wp-content/themes/wolpe/template-parts/retrieve-blog.php
@@ -0,0 +1,8 @@
+<img class="retrieve-header-image" src="<?= the_post_thumbnail_url() ?>" alt="Blog Image">
+<wrapper>
+    <div class="retrieve-content">
+        <p class="title"><?php the_title() ?></p>
+        <p class="paragraph"><?php the_content() ?></p>
+        <p class="info"><?php echo get_the_date() ?> • <span class="blog-categoria"><?php echo get_the_category()[0]->name ?></span></p>
+    </div>
+</wrapper>
