40b8c59dd9ea540e1a483430aabc9def74db30cf
yago batista da silva <yagobatistasilva@gmail.com>
2019-08-15 21:57:55 -0300

    paginas de single

---
+++ wp-content/themes/wolpe/template-parts/retrieve-tratamentos.php
@@ -0,0 +1,7 @@
+<wrapper>
+    <div class="retrieve-content">
+        <img src="<?= the_post_thumbnail_url() ?>" alt="Ansiedade Icon">
+        <p class="title"><?php the_title() ?></p>
+        <p class="paragraph"><?php the_content() ?></p>
+    </div>
+</wrapper>
