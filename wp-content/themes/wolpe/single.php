c2837f695ed11fbc09accb0c2c6c3600609d51a4
yago batista da silva <yagobatistasilva@gmail.com>
2019-08-15 21:57:55 -0300

    paginas de single

--- wp-content/themes/wolpe/single.php
+++ wp-content/themes/wolpe/single.php
@@ -6,25 +6,26 @@
  *
  * @package wolpe
  */
-
+$object = get_post_type_object(get_post_type())->labels;
+$section_name = $object->name;
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="content-area retrieve retrieve-<?= $section_name?>">
 		<main id="main" class="site-main">
 			<wrapper>
 			<?php
 			while ( have_posts() ) :
 				the_post();
 
-				get_template_part( 'template-parts/content', get_post_type() );
+				get_template_part( 'template-parts/retrieve', get_post_type() );
 
-				// the_post_navigation();
+				// // the_post_navigation();
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() && get_comments_number() ) :
-					comments_template();
-				endif;
+				// // If comments are open or we have at least one comment, load up the comment template.
+				// if ( comments_open() && get_comments_number() ) :
+				// 	comments_template();
+				// endif;
 
 			endwhile; // End of the loop.
 			?>
