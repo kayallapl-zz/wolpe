fb4654f5047aae1597ccdd055e1c7fa95862b479
Kayalla <kayallapl@id.uff.br>
2019-08-25 20:30:56 -0300

    adjust

--- wp-content/themes/wolpe/template-parts/retrieve-tratamentos.php
+++ wp-content/themes/wolpe/template-parts/retrieve-tratamentos.php
@@ -1,6 +1,6 @@
 <wrapper>
     <div class="retrieve-content">
-<? if (the_post_thumbnail_url()){ ?><img src="<?= the_post_thumbnail_url() ?>" alt="Ansiedade Icon"><? } ?>
+    <?php if (has_post_thumbnail()){ ?><img src="<?= the_post_thumbnail_url() ?>" alt="Ansiedade Icon"><?php } ?>
         <p class="title"><?php the_title() ?></p>
         <p class="paragraph"><?= force_balance_tags(get_the_content()) ?></p>
     </div>
