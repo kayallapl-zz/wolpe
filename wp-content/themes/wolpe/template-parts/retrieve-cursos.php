ef190e1b174cb04b2b7cdeecaf1c7c8a3137de6c
Kayalla <kayallapl@id.uff.br>
2019-10-05 23:37:56 -0300

    adjustments course date

--- wp-content/themes/wolpe/template-parts/retrieve-cursos.php
+++ wp-content/themes/wolpe/template-parts/retrieve-cursos.php
@@ -2,7 +2,7 @@
 <wrapper>
     <div class="retrieve-content">
         <p class="title"><?php the_title() ?></p>
-        <p class="date"><?php echo get_the_date() ?></p>
+        <p class="date"><?php the_field('data_do_curso') ?></p>
         <p class="paragraph"><?= force_balance_tags(get_the_content()) ?></p>
     </div>
 </wrapper>
