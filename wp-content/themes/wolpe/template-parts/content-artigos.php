6a8b4a7d5ccb2e24e59717162a30413ed71ca37b
Kayalla <kayallapl@id.uff.br>
2019-10-06 00:12:12 -0300

    reverting

--- wp-content/themes/wolpe/template-parts/content-artigos.php
+++ wp-content/themes/wolpe/template-parts/content-artigos.php
@@ -2,7 +2,7 @@
     <div class="card">
         <img src="<?= the_post_thumbnail_url() ?>" alt="Artigos Image">
         <div class="card-content">
-            <p class="info"><?php the_date() ?></p>
+            <p class="info"><?php echo get_the_date() ?></p>
             <p class="title"><?php the_title() ?></p>
             <p class="paragraph"><?= wp_trim_words(get_the_content(), 15, '...') ?></p>
         </div>
